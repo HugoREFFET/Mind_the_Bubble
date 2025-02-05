57bd02297f9680b610c0bffe82a5d588935b2f92
JoblessSlime <raphael.thierry@e-artsup.net>
2025-02-05 20:30:42 +0100

    la création de comic marche bien

--- Assets/Scripts/PersonalizedComic.cs
+++ Assets/Scripts/PersonalizedComic.cs
@@ -5,29 +5,83 @@ public class PersonalizedComic : MonoBehaviour
 {
     public TextMeshProUGUI button_text;
 
-    private GameObject selectedZone;
-    private GameObject selectedComic;
-    private bool zoneIsSelected = false;
+    public GameObject zone1;
+    public GameObject zone2;
+    public GameObject zone3;
+
+    public GameObject selectionNumber_Prefab;
+
+    public GameObject validateComicButton;
+
+    private GameObject selectedComic1, selectedComic2, selectedComic3;
+    private GameObject selectionNumber1, selectionNumber2, selectionNumber3;
+
+    private int comicSelectedNumber = 1;
+
     private bool comics_collection_window_opened = false;
 
-    public void SelectZone(GameObject gameObjectClicked)
+    private GameObject zone_comic1, zone_comic2, zone_comic3;
+
+    private void Awake()
     {
-        Debug.Log("zone Clicked");
-        selectedZone = gameObjectClicked;
-        zoneIsSelected = true;
+        zone_comic1 = zone1.transform.GetChild(0).gameObject;
+        zone_comic2 = zone2.transform.GetChild(0).gameObject;
+        zone_comic3 = zone3.transform.GetChild(0).gameObject;
     }
 
     public void SelectComic(GameObject gameObjectClicked)
     {
-        selectedComic = gameObjectClicked.transform.GetChild(0).gameObject;
-        if (zoneIsSelected)
+        if (comicSelectedNumber == 1)
         {
-            Destroy(selectedZone.transform.GetChild(0).gameObject);
-            GameObject newComicChild = Instantiate(selectedComic, selectedZone.transform);
-            newComicChild.transform.GetChild(1).gameObject.SetActive(true);
-            Debug.Log("it worked ?");
+            selectedComic1 = gameObjectClicked.transform.GetChild(0).gameObject;
+            // Instantiate number as child of selectedComic1
+            selectionNumber1 = Instantiate(selectionNumber_Prefab, selectedComic1.transform);
+            selectionNumber1.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = comicSelectedNumber.ToString();
+        }
+        else if (comicSelectedNumber == 2) 
+        {
+            selectedComic2 = gameObjectClicked.transform.GetChild(0).gameObject;
+
+            selectionNumber2 = Instantiate(selectionNumber_Prefab, selectedComic2.transform);
+            selectionNumber2.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = comicSelectedNumber.ToString();
+        }
+        else if (comicSelectedNumber >= 3)
+        {
+            selectedComic3 = gameObjectClicked.transform.GetChild(0).gameObject;
+
+            selectionNumber3 = Instantiate(selectionNumber_Prefab, selectedComic3.transform);
+            selectionNumber3.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = comicSelectedNumber.ToString();
+
+            validateComicButton.SetActive(true);
         }
 
+        comicSelectedNumber++;
+    }
+
+    public void ValidateComic()
+    {
+        // Destroy number as child of selectedComic1
+        Destroy(selectionNumber1);
+        Destroy(selectionNumber2);
+        Destroy(selectionNumber3);
+
+        Destroy(zone_comic1);
+        Destroy(zone_comic2);
+        Destroy(zone_comic3);
+
+        zone_comic1 = Instantiate(selectedComic1, zone1.transform);
+        zone_comic2 = Instantiate(selectedComic2, zone2.transform);
+        zone_comic3 = Instantiate(selectedComic3, zone3.transform);
+
+        zone_comic1.transform.GetChild(1).gameObject.SetActive(true);
+        zone_comic2.transform.GetChild(1).gameObject.SetActive(true);
+        zone_comic3.transform.GetChild(1).gameObject.SetActive(true);
+
+        comicSelectedNumber = 1;
+        // setActive(false) validateComicButton
+        validateComicButton.SetActive(false);
+
+        Debug.Log("it worked ?");
     }
 
     public void OpenClose_collection(GameObject comics_Collection_Window)
@@ -42,7 +96,6 @@ public class PersonalizedComic : MonoBehaviour
         {
             comics_Collection_Window.SetActive(false);
             button_text.text = "Open collection";
-            zoneIsSelected = false;
         }
     }
 }
